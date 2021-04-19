9241442ac25280f4f7104bac68e23b63769dd51a
FAMOZ-KIMBUMHEE <kog3312@famoz.co.kr>
2021-04-19 18:40:08 +0900

    상품 에디터 개발중

--- src/pages/Admin/Contents/product/EditProduct/index.jsx
+++ src/pages/Admin/Contents/product/EditProduct/index.jsx
@@ -1,7 +1,45 @@
+import CheckBoxLabel from 'compononets/Form/CheckBoxLabel';
 import React from 'react';
+import styled from 'styled-components';
+import 'antd/dist/antd.css';
+import QuestionCircleOutlined from '@ant-design/icons/QuestionCircleOutlined';
+
+const ProduceTitle = styled.div`
+width: 100%;
+height : 10rem;
+background-color : #ffffff;
+border : 1px solid gray;
+padding-left : 40px;
+`;
+
+const TitleTexts = styled.div`
+display : flex;
+`;
+
+const TitleText = styled.div`
+font-size : 25px;
+`;
+
+const iconSize = styled.div`
+font-size : 25px;
+`;
+
+const Test =  styled(QuestionCircleOutlined)`
+  font-size :50px;
+`
 
 const EditProduct = () => {
-  return <div>Edit Product</div>;
+  return <ProduceTitle>
+    <TitleTexts>
+      <TitleText>상품 조회/수정 </TitleText>
+      <iconSize>
+
+      </iconSize>
+    <Test />
+    </TitleTexts>
+  {/* <CheckBox label = "h"/> */}
+  </ProduceTitle>
+
 };
 
 export default EditProduct;
