19013f5ba2cd2199cdb2c4bcdb3a0be38d6ca03e
soohanlee <soohan0808@gmail.com>
2021-05-04 00:55:22 +0900

    feat: wiswig 컴포넌트 생성 및 연결

--- src/pages/Admin/Contents/product/RegisterProduct/collapse/DetailedDescription.jsx
+++ src/pages/Admin/Contents/product/RegisterProduct/collapse/DetailedDescription.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CustomCollapse from 'pages/Admin/components/Collapse';
+import Editor from 'pages/Admin/components/Editor';
+import { EditorState } from 'draft-js';
 
 const DetailedDescription = () => {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const [uploadedImages, setUploadedImages] = useState([]);
+
+  const handleEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
+  const uploadImageCallBack = (file) => {
+    let newUploadedImages = uploadedImages;
+
+    const imageObject = {
+      file: file,
+      localSrc: URL.createObjectURL(file),
+    };
+
+    newUploadedImages.concat(imageObject);
+
+    setUploadedImages(newUploadedImages);
+
+    return new Promise((resolve, reject) => {
+      resolve({ data: { link: imageObject.localSrc } });
+    });
+  };
+
   return (
     <CustomCollapse header="상세설명" extra={'뭔가옴'}>
-      DetailedDescription
+      <Editor
+        editorState={editorState}
+        onEditorStateChange={handleEditorStateChange}
+        uploadImageCallBack={uploadImageCallBack}
+      />
     </CustomCollapse>
   );
 };
