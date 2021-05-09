01cebe94c0b537b32599d1ddb3354afba4e1b94e
LUS <jamborl11@gmail.com>
2021-05-10 01:19:56 +0900

    baseurl 설정

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const { REACT_APP_BASE_URL: baseUrl } = process.env
+
 const Container = styled.div`
 
 `
 
 const Main = () => {
-  return <Container>main</Container>;
+  return <Container>
+    main
+    <br />
+    BASE_URL : {baseUrl}
+  </Container>;
 };
 
 export default Main;
