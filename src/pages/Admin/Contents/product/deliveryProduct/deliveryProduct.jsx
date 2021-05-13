57ad3d5680e0370825095148f47fb8c7af65ba10
FAMOZ-KIMBUMHEE <kog3312@famoz.co.kr>
2021-05-13 18:34:17 +0900

    배송관리 API 작업중

--- src/pages/Admin/Contents/product/deliveryProduct/deliveryProduct.jsx
+++ src/pages/Admin/Contents/product/deliveryProduct/deliveryProduct.jsx
@@ -1,18 +1,55 @@
+import {useState } from 'react';
 import 'antd/dist/antd.css';
 import EditDeliveryProductSetting from 'pages/Admin/Contents/product/deliveryProduct/editDeliveryProductSetting';
 import EditDeliveryProductCategory from 'pages/Admin/Contents/product/deliveryProduct/editDeliveryProductCategory';
 import EditDeliveryProductTitle from 'pages/Admin/Contents/product/deliveryProduct/editDeliveryProductTitle';
 import EditDeliveryProductTable from 'pages/Admin/Contents/product/deliveryProduct/editDeliveryProductTable';
 
-const deliveryProduct = () => {
+import { allDeliveryProduct, searchDeliveryProduct } from 'apis/delivery';
+import { notification } from 'utils/notification';
+
+
+
+const DeliveryProduct = () => {
+  const [tableData, setTableData] = useState([]);
+
+  const getAllDeliveryData = async() => {
+    try{
+      const result = await allDeliveryProduct();
+      setTableData(result.data.data.list);
+    } catch(e){
+      notification.error('배송 정보를 가져오지 못했습니다.');
+    }
+  };
+
+  const getSearchDeliveryData = async(id) => {
+    try{
+      const result = await searchDeliveryProduct(id);
+      setTableData(result.data.data);
+      console.log(result.data.data);
+
+
+    } catch(e){
+      notification.error('배송 정보를 가져오지 못했습니다.');
+    }
+  };
+
+  const initDeliveryData = async() => {
+    try{
+      setTableData('');
+    } catch(e){
+      notification.error('배송 정보를 초기화하지 못했습니다.');
+    }
+  };
+
   return (
     <>
       <EditDeliveryProductTitle />
       <EditDeliveryProductCategory />
-      <EditDeliveryProductSetting />
-      <EditDeliveryProductTable />
+      <EditDeliveryProductSetting initDeliveryData = {initDeliveryData} getAllDeliveryData = {getAllDeliveryData} getSearchDeliveryData = {getSearchDeliveryData} />
+      <EditDeliveryProductTable result = {tableData} />
     </>
   );
 };
 
-export default deliveryProduct;
+export default DeliveryProduct;
