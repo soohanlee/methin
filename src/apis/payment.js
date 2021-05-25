1f278839bfef3dc1906dd637035eb51e47dcea3e
soohanlee <soohan0808@gmail.com>
2021-05-25 21:33:51 +0900

    payment api

--- src/apis/payment.js
+++ src/apis/payment.js
@@ -17,6 +17,27 @@ export const getPaymentUnpaidList = async (data) => {
 };
 
 // 결제 완료 + 상품 준비중 주문 목록
-export const getPaidWithPaymentConfirmed = async (data) => {
+export const getPaidWithPaymentConfirmedList = async (data) => {
+  // - offset: number (default: 0, optional) ⇒ 페이지 번호
+  // - limit: number (default: 16, optional) ⇒ 페이지별 개수
   return await axios.get(`${baseUrl}/paid-or-payment-confirmed`, data);
+};
+
+// 배송중 주문목록 => 배송현황 탭
+export const getShipConfirmedList = async (data) => {
+  // - offset: number (default: 0, optional) ⇒ 페이지 번호
+  // - limit: number (default: 16, optional) ⇒ 페이지별 개수
+  return await axios.get(`${baseUrl}/ship-confirmed`);
+};
+
+export const getCanceledPaymentList = async (data) => {
+  // - offset: number (default: 0, optional) ⇒ 페이지 번호
+  // - limit: number (default: 16, optional) ⇒ 페이지별 개수
+  return await axios.get(`${baseUrl}/canceled`, data);
+};
+
+export const getRefundedPaymentList = async (data) => {
+  // - offset: number (default: 0, optional) ⇒ 페이지 번호
+  // - limit: number (default: 16, optional) ⇒ 페이지별 개수
+  return await axios.get(`${baseUrl}/refunded`, data);
 };
