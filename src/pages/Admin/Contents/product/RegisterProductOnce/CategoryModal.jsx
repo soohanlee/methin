b578b2c5789f7d29b54436580e1104f1b68ffa64
Famoz <kog3312@famoz.co.kr>
2021-06-06 01:38:10 +0900

    상품일괄등록 코드 정리

--- src/pages/Admin/Contents/product/RegisterProductOnce/CategoryModal.jsx
+++ src/pages/Admin/Contents/product/RegisterProductOnce/CategoryModal.jsx
@@ -3,47 +3,46 @@ import { Modal } from 'antd';
 import styled from 'styled-components';
 import BasicTextInputBox from 'pages/Admin/components/Form/BasicTextInputBox';
 import BasicButton from 'pages/Admin/components/Form/BasicButton';
-import 'antd/dist/antd.css';
 import { useEffect, useState } from 'react';
 
-const ItemContainer = styled.div`
+const ItemContainerStyled = styled.div`
   display: flex;
 `;
 
-const ItemBox = styled.div`
+const SubContainerStyled = styled.div`
   max-width: 30rem;
   width: 100%;
   border: 1px solid black;
   margin-right: 1rem;
 `;
 
-const ItemHeader = styled.div`
-  border-bottom: 0.1rem solid black;
-`;
-
-const ItemBody = styled.div`
+const SubItemContainerStyled = styled.div`
   height: 30rem;
   overflow-y: auto;
 `;
 
-const Item = styled.div`
+const ItemStyled = styled.div`
   background: ${(props) => (props.isClicked ? 'lightgrey' : 'white')};
 `;
 
-const CategoryModalBox = styled.div`
+const ItemTitleStyled = styled.div`
+  border-bottom: 0.1rem solid black;
+`;
+
+const Styled = styled.div`
   padding: 2rem;
   padding-bottom: 0px;
   border: 1px solid #f0f0f0;
 `;
 
-const CategoryModalContent = styled.div`
+const ContainerStyled = styled.div`
   width: 100%;
   display: flex;
   margin-bottom: 2rem;
   align-items: center;
 `;
 
-const ContentTitle = styled.div`
+const TitleTextStyled = styled.div`
   margin-right: 3rem;
 `;
 
@@ -53,23 +52,22 @@ const BasicTextInputBoxStyled = styled(BasicTextInputBox)`
 `;
 
 const CategoryModal = (property) => {
-  const [categoryIndex, setCategoryIndex] = useState(0);
-
-  const [data, setData] = useState({});
-  const [selectedFirstItem, setSelectedFirstItem] = useState('축산');
-  const [selectedSecondItem, setSelectedSecondItem] = useState('축산');
-  const dataKey = Object.keys(data);
+  const [categoryTypeState, setCategoryTypeState] = useState(0);
+  const [classificationdataState, setClassificationdataState] = useState({});
+  const [selectedFirstItemState, setSelectedFirstItemState] = useState('축산');
+  const [selectdSecondItemState, setSelectedSecondItemState] = useState('축산');
+  const dataKey = Object.keys(classificationdataState);
 
   useEffect(() => {
-    setData(property.dataList);
+    setClassificationdataState(property.dataList);
   }, []);
 
   const handleFristItemClick = (value) => {
-    setSelectedFirstItem(value);
+    setSelectedFirstItemState(value);
   };
 
   const handleSecondItemClick = (value) => {
-    setSelectedSecondItem(value);
+    setSelectedSecondItemState(value);
   };
 
   const okClick = () => {
@@ -78,98 +76,72 @@ const CategoryModal = (property) => {
   };
 
   const renderTap = () => {
-    switch (categoryIndex) {
+    switch (categoryTypeState) {
       case 0:
-        return renderCategory0();
+        return renderCategorySearch();
       case 1:
-        return renderCategory1();
-      case 2:
-        return renderCategory2();
+        return renderCategorySelect();
       default:
-        return renderCategory0();
+        return renderCategorySearch();
     }
   };
 
-  const renderCategory0 = () => {
+  const renderCategorySearch = () => {
     return (
       <>
-        <CategoryModalContent>
-          <ContentTitle>카테고리명</ContentTitle>
+        <ContainerStyled>
+          <TitleTextStyled>카테고리명</TitleTextStyled>
           <BasicTextInputBoxStyled
             ref={property.catagoryRef}
           ></BasicTextInputBoxStyled>
-        </CategoryModalContent>
+        </ContainerStyled>
 
-        <CategoryModalContent>
-          <ContentTitle>선택한 카테고리 :</ContentTitle>
-        </CategoryModalContent>
+        <ContainerStyled>
+          <TitleTextStyled>선택한 카테고리 :</TitleTextStyled>
+        </ContainerStyled>
 
-        <CategoryModalContent>
-          <ContentTitle>선택한 카테고리 코드 : </ContentTitle>
-        </CategoryModalContent>
+        <ContainerStyled>
+          <TitleTextStyled>선택한 카테고리 코드 : </TitleTextStyled>
+        </ContainerStyled>
       </>
     );
   };
 
-  const renderCategory1 = () => {
+  const renderCategorySelect = () => {
     return (
       <>
-        <ItemContainer>
-          <ItemBox>
-            <ItemHeader>1차 분류</ItemHeader>
-            <ItemBody>
+        <ItemContainerStyled>
+          <SubContainerStyled>
+            <ItemTitleStyled>1차 분류</ItemTitleStyled>
+            <SubItemContainerStyled>
               {dataKey &&
                 dataKey.map((item) => (
-                  <Item
+                  <div
                     key={item}
-                    isClicked={item === selectedFirstItem}
+                    isClicked={item === selectedFirstItemState}
                     onClick={() => handleFristItemClick(item)}
                   >
                     {item}
-                  </Item>
+                  </div>
                 ))}
-            </ItemBody>
-          </ItemBox>
-          <ItemBox>
-            <ItemHeader>2차 분류</ItemHeader>
-            <ItemBody>
-              {data[selectedFirstItem] &&
-                data[selectedFirstItem].map((item) => (
-                  <Item
+            </SubItemContainerStyled>
+          </SubContainerStyled>
+          <SubContainerStyled>
+            <ItemTitleStyled>2차 분류</ItemTitleStyled>
+            <SubItemContainerStyled>
+              {classificationdataState[selectedFirstItemState] &&
+                classificationdataState[selectedFirstItemState].map((item) => (
+                  <ItemStyled
                     key={item}
-                    isClicked={item === selectedSecondItem}
+                    isClicked={item === selectdSecondItemState}
                     onClick={() => handleSecondItemClick(item)}
                   >
                     {item}
-                  </Item>
+                  </ItemStyled>
                 ))}
-            </ItemBody>
-          </ItemBox>
-        </ItemContainer>
-      </>
-    );
-  };
-
-  const renderCategory2 = () => {
-    return (
-      <>
-        <CategoryModalContent>
-          <ContentTitle>카테고리명</ContentTitle>
-          <BasicTextInputBoxStyled
-            onClick={(e) => {
-              console.log();
-            }}
-            ref={property.catagoryRef}
-          ></BasicTextInputBoxStyled>
-        </CategoryModalContent>
-
-        <CategoryModalContent>
-          <ContentTitle>선택한 카테고리 :</ContentTitle>
-        </CategoryModalContent>
-
-        <CategoryModalContent>
-          <ContentTitle>선택한 카테고리 코드 : </ContentTitle>
-        </CategoryModalContent>
+            </SubItemContainerStyled>
+          </SubContainerStyled>
+        </ItemContainerStyled>
       </>
     );
   };
@@ -187,29 +159,23 @@ const CategoryModal = (property) => {
         okText="저장"
         cancelText="닫기"
       >
-        <CategoryModalBox>
-          <CategoryModalContent>
+        <ContainerStyled>
+          <ContainerStyled>
             <BasicButton
               onClick={() => {
-                setCategoryIndex(0);
+                setCategoryTypeState(0);
               }}
               label="카테고리명 검색"
             ></BasicButton>
             <BasicButton
               onClick={() => {
-                setCategoryIndex(1);
+                setCategoryTypeState(1);
               }}
               label="카테고리명 선택"
             ></BasicButton>
-            {/* <BasicButton
-              onClick={() => {
-                setCategoryIndex(2);
-              }}
-              label="카테고리 템플릿"
-            ></BasicButton> */}
-          </CategoryModalContent>
+          </ContainerStyled>
           {renderTap()}
-        </CategoryModalBox>
+        </ContainerStyled>
       </Modal>
     </>
   );
