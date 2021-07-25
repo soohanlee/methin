bb0b1e446a24884cd1395fb025fbce44a1494e00
soohan lee <soohan0808@famoz.co.kr>
2021-07-25 15:48:52 +0900

    Empty Container 생성

--- src/components/EmptyContainer/index.jsx
+++ src/components/EmptyContainer/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
